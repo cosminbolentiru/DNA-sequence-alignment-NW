2ed9f41908afe956f7b27451adfb1e4cc3370db8
Cosmin Bolentiru <cos.bolentiru@gmail.com>
2017-08-10 15:43:56 -0400

    Organized comments

--- seq_alignment.py
+++ seq_alignment.py
@@ -1,4 +1,4 @@
-# Global constants that can be changed for preferance 
+# Global constants that can be changed for preferance
 match = 1
 mismatch = -1
 gap = -1
@@ -8,18 +8,11 @@ gap = -1
 seq_1 = 'AGGTCACCT'
 seq_2 = 'GGCACGT'
 
-A C T T A 
-C T A 
-
-A C T T A
-  C T   A
-  C   T A
-
 # EXAMPLE OF 2D ARRAY:
 #
 #     A  G  G  T  C  A  C  C  T
 #   0-1 -2 -3 -4 -5 -6 -7 -8 -9
-# G-1 
+# G-1
 # G-2
 # C-3
 # A-4
@@ -54,7 +47,7 @@ def diagnaol_val(element, pos_1, pos_2, seq_1, seq_2):
         new_d = (element + mismatch)
         return new_d
 
-# main function 
+# main function
 def seq_align(seq1, seq2):
     len_1 = len(seq1)
     len_2 = len(seq2)
@@ -62,9 +55,9 @@ def seq_align(seq1, seq2):
     left_y = base_list(len_2)
     lst_seq1 = seq_list(seq1)
     lst_seq2 = seq_list(seq2)
-    
+
     # ///////////////////////////////////////////////
-    # new try for printing the filled matirx 
+    # new try for printing the filled matirx
     # fill matrix with all 0's
         # each element will be list of len(seq2)+1
     matrix = []
@@ -84,11 +77,11 @@ def seq_align(seq1, seq2):
     y = 1
     while x < len_1:
         while y <=len_2:
-            element = matrix[y-1][x-1] 
+            element = matrix[y-1][x-1]
             d_val = diagnaol_val(element, pos_1, pos_2, seq_1, seq_2)
             up = (matrix[(y-1)][x] + gap)
             left = (matrix[y][(x-1)] + gap)
-            #choose best value 
+            #choose best value
             replacement = max(d_val, up, left)
             matrix[y][x] = replacement
             y += 1
@@ -97,10 +90,10 @@ def seq_align(seq1, seq2):
         x += 1
         pos_2 = 0
         pos_1 += 1
-        
-    # prints 2-D grid     
+
+    # prints 2-D grid
     for i in range(len(matrix)):
-            print(matrix[i])    
+            print(matrix[i])
 
 
 # END OF FUNCTION
