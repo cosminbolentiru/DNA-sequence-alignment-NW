2fc2f7cdb7bf6c5f601f07cb715a16e221aced0f
Cosmin Bolentiru <cos.bolentiru@gmail.com>
2017-08-10 15:25:13 -0400

    Changed title

--- seq_alignment.py
+++ seq_alignment.py
@@ -1,31 +1,27 @@
-# Global constants that can be changed for preferance 
-match = 1
-mismatch = -1
-gap = -1
-
-#    str1 and str2 must be made up of 'A', 'B', 'C', 'D'
-
-seq_1 = 'AGGTCACCT'
-seq_2 = 'GGCACGT'
-
-A C T T A 
-C T A 
-
-A C T T A
-  C T   A
-  C   T A
-
+# This file contains all the global variables, helper functions and main
+#   function that will implement NW Global alignment algorithm
+#
+# The following is an example of the format that will be printed
 # EXAMPLE OF 2D ARRAY:
 #
 #     A  G  G  T  C  A  C  C  T
 #   0-1 -2 -3 -4 -5 -6 -7 -8 -9
-# G-1 
+# G-1
 # G-2
 # C-3
 # A-4
 # C-5
 # G-6
 # T-7
+
+# Global constants that can be changed for preferance
+match = 1
+mismatch = -1
+gap = -1
+
+# Enter the two sequences to be aligned here 
+seq_1 = 'AGGTCACCT'
+seq_2 = 'GGCACGT'
 
 # produces a list of letters (list = one seq)
 def seq_list(seq):
@@ -42,8 +38,8 @@ def base_list(length):
         num_lst = num_lst + [i * -1]
     return num_lst
 
-
-# diag function
+# will produce the value of the diagnol test whil comparing values
+#   within the matrix
 def diagnaol_val(element, pos_1, pos_2, seq_1, seq_2):
     # if there is a match
     if seq_1[pos_1] == seq_2[pos_2]:
@@ -54,7 +50,7 @@ def diagnaol_val(element, pos_1, pos_2, seq_1, seq_2):
         new_d = (element + mismatch)
         return new_d
 
-# main function 
+# main function
 def seq_align(seq1, seq2):
     len_1 = len(seq1)
     len_2 = len(seq2)
@@ -62,11 +58,7 @@ def seq_align(seq1, seq2):
     left_y = base_list(len_2)
     lst_seq1 = seq_list(seq1)
     lst_seq2 = seq_list(seq2)
-    
-    # ///////////////////////////////////////////////
-    # new try for printing the filled matirx 
-    # fill matrix with all 0's
-        # each element will be list of len(seq2)+1
+
     matrix = []
     for i in range(len_2 + 1):
         if i == 0:
@@ -84,11 +76,11 @@ def seq_align(seq1, seq2):
     y = 1
     while x < len_1:
         while y <=len_2:
-            element = matrix[y-1][x-1] 
+            element = matrix[y-1][x-1]
             d_val = diagnaol_val(element, pos_1, pos_2, seq_1, seq_2)
             up = (matrix[(y-1)][x] + gap)
             left = (matrix[y][(x-1)] + gap)
-            #choose best value 
+            #choose best value
             replacement = max(d_val, up, left)
             matrix[y][x] = replacement
             y += 1
@@ -97,11 +89,11 @@ def seq_align(seq1, seq2):
         x += 1
         pos_2 = 0
         pos_1 += 1
-        
-    # prints 2-D grid     
+
+    # prints 2-D grid
     for i in range(len(matrix)):
-            print(matrix[i])    
-
-
+            print(matrix[i])
 # END OF FUNCTION
+
+# Tests
 seq_align(seq_1, seq_2)
